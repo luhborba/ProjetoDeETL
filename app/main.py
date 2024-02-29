39560351c3f545c4bbe2ed1e45baebe1e519ea33
Luciano Borba <luhborbafilho@gmail.com>
2024-02-29 15:29:57 -0300

    feat(load): finalizando load e inserindo etl completo na main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
 from pipeline.extract import extranindo_dados_excel
+from pipeline.trasform import concatenar_df
+from pipeline.load import load_excel
 
-lista_df = extranindo_dados_excel("data/input")
-print(lista_df)
+if __name__ == "__main__":
+    path = "data/input"
+    lista_df = extranindo_dados_excel(path)
+    df = concatenar_df(lista_df)
+    load_excel(df, "data/output", "output")
